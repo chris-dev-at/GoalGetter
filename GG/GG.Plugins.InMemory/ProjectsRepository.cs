0fd454a692d7b6e6b7cb2e7bd6135b40b7245d61
Mulham Taylouni <mulhamsth@gmail.com>
2023-05-09 16:23:46 +0200

    updating the logic

--- GG/GG.Plugins.InMemory/ProjectsRepository.cs
+++ GG/GG.Plugins.InMemory/ProjectsRepository.cs
@@ -12,17 +12,20 @@ namespace GG.Plugins.InMemory
 		{
 			CreateTestData();
 		}
-		public Task<IEnumerable<Project>> GetProjectsByNameAsync(string name)
+		public async Task<IEnumerable<Project>> GetProjectsByNameAsync(string name)
 		{
-			throw new NotImplementedException();
+			if (string.IsNullOrWhiteSpace(name))
+				return await Task.FromResult(Projects);
+			return Projects.Where(x => x.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
 		}
+		Random randy = new Random();
 
 
-		public void CreateTestData()
+        void CreateTestData()
 		{
 			Contact.Add(new Person()
 			{
-				Id = "0K9P3G9V",
+				Id = RandomString(),
 				Firstname = "Rainer",
 				Lastname = "Winkler",
 				Address = "DE, Emskirchen 91448, Altschauerberg 8",
@@ -32,7 +35,7 @@ namespace GG.Plugins.InMemory
 
 			Contact.Add(new Person()
 			{
-				Id = "DE2876YI",
+				Id = RandomString(),
 				Firstname = "Ilse",
 				Lastname = "Nigischer",
 				Address = "AT Waidhofen a. T. 3830, Buxdihudenstraße 1",
@@ -42,7 +45,7 @@ namespace GG.Plugins.InMemory
 
 			Contact.Add(new Person()
 			{
-				Id = "M8A557WC",
+				Id = RandomString(),
 				Firstname = "Herwig",
 				Lastname = "Macho",
 				Address = "AT Zwettl 3910, Propstei 8",
@@ -52,7 +55,7 @@ namespace GG.Plugins.InMemory
 
 			Contact.Add(new Person()
 			{
-				Id = "QMRBJH1U",
+				Id = RandomString(),
 				Firstname = "Lukas",
 				Lastname = "Kolinsky",
 				Address = "AT, Zwettl 3910, Propstei 7",
@@ -62,27 +65,27 @@ namespace GG.Plugins.InMemory
 
 			Contact.Add(new Person()
 			{
-				Id = "WLA9PUCX",
+				Id = RandomString(),
 				Firstname = "Mulham",
 				Lastname = "Taylouni",
-				Address = "AT, Gmünd 3950, Schremserstraße 51",
+				Address = "AT, Gmünd 3950, Schremserstraße 69",
 				AvatarPath = @"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
 				Email = "contact@taylouni.me"
 			});
 
 			Contact.Add(new Person()
 			{
-				Id = "AHR44LMZ",
+				Id = RandomString(),
 				Firstname = "Christian",
 				Lastname = "Wiesinger",
-				Address = "AT, Rappottenstein 3911, Marbach am Walde 79",
+				Address = "AT, Rappottenstein 3911, Burgunderweg 109",
 				AvatarPath = @"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
 				Email = "contact@taylouni.me"
 			});
 
 			Contact.Add(new Person()
 			{
-				Id = "AHR44LMZ",
+				Id = RandomString(),
 				Firstname = "Clemens",
 				Lastname = "Schmid",
 				Address = "AT, Zwettl 3910,Hammerweg 3",
@@ -92,31 +95,69 @@ namespace GG.Plugins.InMemory
 
 
 			//Project I
-			Team t1 = new Team() { Description = "Dream Team" };
+			Team t1 = new Team() { Description = "Dream Team", Id = RandomString(), members = new List<Teammember>() };
 
 			t1.members.Add(new Teammember() { person = getRandomPerson(), Description = "Designer", Role = Teamrolle.Worker });
 			t1.members.Add(new Teammember() { person = getRandomPerson(), Description = "Programmer", Role = Teamrolle.Worker });
 			t1.members.Add(new Teammember() { person = getRandomPerson(), Description = "Projektleiter", Role = Teamrolle.Projektleader });
 
-			Project p1 = new Project() { Name = "Project 1", Budget = 99.50f, status = ProgressStatus.In_Progress, assignedTeam = t1 };
+			Project p1 = new Project() { Name = "Project 1", Budget = 99.50f, status = ProgressStatus.In_Progress, assignedTeam = t1, Description = "This project is for testing", Id = RandomString(), Tasks = new List<ProjectTask>() };
+
+			p1.Tasks.Add(new ProjectTask() { Name = "Code Website", Description = "Make a website lol. xd. mega lol.", AssignedPerson = getRandomTPerson(t1), Deadline = RandomDay(), Id = RandomString() });
+			p1.Tasks.Add(new ProjectTask() { Name = "Code Website 2", Description = "Make a website lol. xd. mega lol.", AssignedPerson = getRandomTPerson(t1), Deadline = RandomDay(), Id = RandomString() });
+			p1.Tasks.Add(new ProjectTask() { Name = "Code Website 3", Description = "Make a website lol. xd. mega lol.", AssignedPerson = getRandomTPerson(t1), Deadline = RandomDay(), Id = RandomString() });
+			p1.Tasks.Add(new ProjectTask() { Name = "Have FreeTime 1", Description = "Have Freetime lol. Very hard to understand", AssignedPerson = getRandomTPerson(t1), Deadline = RandomDay(), Id = RandomString() });
+			p1.Tasks.Add(new ProjectTask() { Name = "Create Blazor App 1", Description = ".NET 7.0 CORE Framework needed. please install it and create blazorproject with it", AssignedPerson = getRandomTPerson(t1), Deadline = RandomDay(), Id = RandomString() });
+			p1.Tasks.Add(new ProjectTask() { Name = "Create Blazor App 2", Description = ".NET 6.0 CORE Framework needed. please install it and create blazorproject with it", AssignedPerson = getRandomTPerson(t1), Deadline = RandomDay(), Id = RandomString() });
+
+			Projects.Add(p1);
 
 
 			//Project II
-			Team t2 = new Team() { Description = "Die 4 netten Menschen" };
+			Team t2 = new Team() { Description = "Die 4 netten Menschen", Id = RandomString(), members = new List<Teammember>() };
 
-			t1.members.Add(new Teammember() { person = getRandomPerson(), Description = "Designer", Role = Teamrolle.Worker });
-			t1.members.Add(new Teammember() { person = getRandomPerson(), Description = "Programmer", Role = Teamrolle.Worker });
-			t1.members.Add(new Teammember() { person = Contact[0], Description = "Programmer", Role = Teamrolle.Administrator });
-			t1.members.Add(new Teammember() { person = getRandomPerson(), Description = "Projektleiter", Role = Teamrolle.Projektleader });
+			t2.members.Add(new Teammember() { person = getRandomPerson(), Description = "Designer", Role = Teamrolle.Worker });
+			t2.members.Add(new Teammember() { person = getRandomPerson(), Description = "Programmer", Role = Teamrolle.Worker });
+			t2.members.Add(new Teammember() { person = Contact[0], Description = "Programmer", Role = Teamrolle.Administrator });
+			t2.members.Add(new Teammember() { person = getRandomPerson(), Description = "Projektleiter", Role = Teamrolle.Projektleader });
 
-			Project p2 = new Project() { Name = "Funky Munky", Budget = 99.50f, status = ProgressStatus.In_Progress, assignedTeam = t2 };
+			Project p2 = new Project() { Name = "Funky Munky", Budget = 99.50f, status = ProgressStatus.In_Progress, assignedTeam = t2, Tasks = new List<ProjectTask>() };
+
+			p2.Tasks.Add(new ProjectTask() { Name = "Code Website", Description = "Make a website lol. xd. mega lol.", AssignedPerson = getRandomTPerson(t2), Deadline = RandomDay(), Id = RandomString() });
+			p2.Tasks.Add(new ProjectTask() { Name = "Code Website 2", Description = "Make a website lol. xd. mega lol.", AssignedPerson = getRandomTPerson(t2), Deadline = RandomDay(), Id = RandomString() });
+			p2.Tasks.Add(new ProjectTask() { Name = "Code Website 3", Description = "Make a website lol. xd. mega lol.", AssignedPerson = getRandomTPerson(t2), Deadline = RandomDay(), Id = RandomString() });
+			p2.Tasks.Add(new ProjectTask() { Name = "Have FreeTime 1", Description = "Have Freetime lol. Very hard to understand", AssignedPerson = getRandomTPerson(t2), Deadline = RandomDay(), Id = RandomString() });
+			p2.Tasks.Add(new ProjectTask() { Name = "Create Blazor App 1", Description = ".NET 7.0 CORE Framework needed. please install it and create blazorproject with it", AssignedPerson = getRandomTPerson(t2), Deadline = RandomDay(), Id = RandomString() });
+			p2.Tasks.Add(new ProjectTask() { Name = "Create Blazor App 2", Description = ".NET 6.0 CORE Framework needed. please install it and create blazorproject with it", AssignedPerson = getRandomTPerson(t2), Deadline = RandomDay(), Id = RandomString() });
+
+			Projects.Add(p2);
 
 		}
 
-		public Person getRandomPerson()
+		//Functions
+		Person getRandomPerson()
 		{
-			Random randy = new Random();
 			return Contact.ToArray()[randy.Next(0, Contact.Count)];
+		}
+		Teammember getRandomTPerson(Team t)
+		{
+			return t.members.ToArray()[randy.Next(0, t.members.Count)];
+		}
+		DateTime RandomDay()
+		{
+			DateTime start = new DateTime(1995, 1, 1);
+			int range = (DateTime.Today - start).Days;
+			return start.AddDays(randy.Next(range));
+		}
+		string RandomString(int length = 8)
+		{
+			string ret = "";
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			ret = new string(Enumerable.Repeat(chars, length)
+					.Select(s => s[randy.Next(s.Length)]).ToArray());
+			ret = new string(Enumerable.Repeat(chars, length)
+				.Select(s => s[randy.Next(s.Length)]).ToArray());
+			return ret;
 		}
 	}
 }
