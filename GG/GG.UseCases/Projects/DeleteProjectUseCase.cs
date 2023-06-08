15ff0fbf78e8b427bf3a0d01bbc89e262b749320
Mulhamsth <mulhamsth@gmail.com>
2023-06-08 17:52:23 +0200

    adjusting design

--- GG/GG.UseCases/Projects/DeleteProjectUseCase.cs
+++ GG/GG.UseCases/Projects/DeleteProjectUseCase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GG.CoreBusiness;
+using GG.UseCases.PluginInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace GG.UseCases.Projects
 {
 	public class DeleteProjectUseCase
 	{
+		private readonly IProjectsRepository projectsRepository;
+		public DeleteProjectUseCase(IProjectsRepository projectsRepository)
+		{
+			this.projectsRepository = projectsRepository;
+		}
+		//public async Task<StatusReport<EmptyVal>> ExecuteAsync(int projectid)
+		//{
+		//	var pro = this.projectsRepository.Re(projectid).Result;
+		//	return projectsRepository.ChangeProject(pro.Value).Result;
+		//}
 	}
 }
